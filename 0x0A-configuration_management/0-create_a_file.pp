c37d10c655571f9f1a2a6290d70b4eb990dcb9fa
wesacoke <wesaemmanuel02@gmail.com>
2024-06-10 05:46:21 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This puppet manifest creates a file names/tmp/school
 
 file { '/tmp/school':
-ensure => present,
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+ensure  => present,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => "I love Puppet\n"
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A puppet manifest installing flask vesion v2.1.0 which is a package from pip3
 
 package {'flask':
-ensre => '2.1.0',
-provider => pip3,
+ensure => '2.1.0',
+provider => 'pip3',
 }
