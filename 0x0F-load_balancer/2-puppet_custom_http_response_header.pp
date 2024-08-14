c9c7edfbde1caca528fa9b0e62144f2a43d8813d
wesacoke <wesaemmanuel02@gmail.com>
2024-08-15 00:18:16 +0300

    0-custom_http_response_header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Install nginx and config the custom HTTP header response
+
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+file_line { 'headercustom':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['headercustom'],
+}
